931fd73d98aafe1af800f2a034a666f30180a61a
Mohamed Rashid <rashivkp@gmail.com>
2017-03-29 01:10:27 +0530

    add migrations insert data to database
    update requirements.txt with new requirements

---
+++ README.md
@@ -0,0 +1,26 @@
+# KTU Result Parser
+
+parse results from pdf and insert them to db.
+
+
+### Requirements
+
+- python >= 2.7
+- mysql
+- sed
+
+### How-to setup
+
+```
+pip install -r requirements.txt
+```
+
+this  will install all the requirements
+
+
+### Run server
+
+```
+python app.py
+```
+
--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 from flask import Flask, flash, render_template, request, redirect, url_for
 import os, re, MySQLdb
 from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['pdf'])
 app = Flask(__name__)
 app.secret_key = 'sdfakjkj'
 app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://pdfparser:password@localhost/pdfdata'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+class Subject(db.Model):
+    __tablename__ = 'subject'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    code = db.Column(db.String(128))
+
+class Result(db.Model):
+    __tablename__ = 'result'
+    id = db.Column(db.Integer, primary_key=True)
+    reg = db.Column(db.String(128))
+    sub1 = db.Column(db.String(8))
+    sub2 = db.Column(db.String(8))
+    sub3 = db.Column(db.String(8))
+    sub4 = db.Column(db.String(8))
+    sub5 = db.Column(db.String(8))
+    sub6 = db.Column(db.String(8))
+    sub7 = db.Column(db.String(8))
+    sub8 = db.Column(db.String(8))
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -44,9 +72,12 @@ def process_pdf(pdf_filename):
         else:
             next_line = ''
 
-    print subjects
-    print students
-    print len(subjects)
+    for s in subjects:
+        db.session.add(Subject(code=s[0], name=s[1]))
+    for st in students:
+        if len(st) == 8:
+            db.session.add(Result(reg=st[0], sub1=st[1], sub2=st[2], sub3=st[3], sub4=st[4], sub5=st[5], sub6=st[6], sub7=st[7]))
+    db.session.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -70,7 +101,7 @@ def hello_world():
         # check if the post request has
     return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.debug = True
-    app.run(debug=app.debug, host='0.0.0.0')
+    manager.run()
 
